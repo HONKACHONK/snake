e7b3ce80ad23f5011a2e4f2fed4ef1339d176310
HONKACHONK <100553453+HONKACHONK@users.noreply.github.com>
2022-05-12 10:59:40 -0600

    tail and conio input

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <thread>
 #include <chrono>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -22,7 +25,33 @@ int score = 1;
 int gameOver = 0;
 int dir = 1; //0=up, 1=right, 2=down, 3=left
 int taildir = 1;
+int init = 0;
 
+
+int keypress() {
+  system ("/bin/stty raw");
+  int c;
+  system ("/bin/stty -echo");
+  c = getc(stdin);
+  system ("/bin/stty echo");
+  system ("/bin/stty cooked");
+	int d;
+	switch(c) {
+		case 119:
+			d = 0;
+			break;
+		case 97:
+			d = 3;
+			break;
+		case 115:
+			d = 2;
+			break;
+		case 100:
+			d = 1;
+			break;
+	}
+  return d;
+}
 
 void setup() {
 	/**
@@ -46,7 +75,7 @@ void draw() {
 	/**
 	draws the board
 	*/
-	cout << "\033[2J\033[0;0H"; //no idea how, but this clears the screen
+	system('cls');
 	for (int i = 0; i < boardX; i++) {
 		for (int j = 0; j < boardY; j++) {
 			if (board[i][j] == 1)
@@ -68,17 +97,41 @@ void logic() {
 	/**
 	performs all logic
 	*/
-	tailboard[headX][headY] = dir;
+	if (init == 0)
+		tailboard[tailX][tailY] = 1;
+			
+	tailboard[headX][headY] = d97ir;
+
+	if (_kbhit()) {
+		switch(_getch()) {
+			case 119:
+				dir = 0;
+				break;
+			case 100:
+				dir = 1;
+				break;
+			case 115:
+				dir = 2;
+				break;
+			case 97:
+				dir = 3;
+				break;
+		}
+	}
 	
 	switch (dir) {
 		case 0:
 			headY++;
+			break;
 		case 1:
 			headX++;
+			break;
 		case 2:
 			headY--;
+			break;
 		case 3:
 			headX--;
+			break;
 	}
 
 	if ((board[headX][headY] == 1) || ((board[headX][headY] == 2) && (board[headX][headY] != 1))) {
@@ -91,24 +144,30 @@ void logic() {
 		appleX = rand() % (boardX - 2) + 1;
 		appleY = rand() % (boardY - 2) + 1;
 	}
-	board[tailX][tailY] = 0;
+	else
+		board[tailX][tailY] = 0;
 	taildir = (tailboard[tailX][tailY] + 1);
 	tailboard[tailX][tailY] = 0;
 		
 	switch (taildir) {
 		case 1:
 			tailY++;
+			break;
 		case 2:
 			tailX++;
+			break;
 		case 3:
 			tailY--;
+			break;
 		case 4:
 			tailX--;
+			break;
 	}
 	
 	board[headX][headY] = 2;
 	
 	board[appleX][appleY] = 3;
+	init = 1;
 }
 
 int main() {
